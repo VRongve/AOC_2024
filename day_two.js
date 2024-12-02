6f45fcc3247a02c3953517a1165eb1a0e80ec44f
VRongve <44428397+VRongve@users.noreply.github.com>
2024-12-02 16:42:30 +0100

    dd

--- day_two.js
+++ day_two.js
@@ -1,3 +1,4 @@
+const { unsubscribe } = require("diagnostics_channel");
 const { reverse } = require("dns");
 
 const fs = require("fs").promises; // Use the promises API
@@ -32,6 +33,21 @@ function isIncreasingOrDecreasing(array) {
   return result;
 }
 
+function isIncreasingOrDecreasingPart2(array) {
+  let result = false;
+  let reversArr = [...array].reverse();
+  let listArrys = [array, reversArr];
+  for (let i = 0; i < listArrys.length; i++) {
+    let value = checkIfArraySafe(listArrys[i]);
+    if (value === true) {
+      result = true;
+      break;
+    }
+  }
+
+  return result;
+}
+
 function checkIfArraySafe(array) {
   let result = true;
   let validNumbers = [1, 2, 3];
@@ -40,6 +56,7 @@ function checkIfArraySafe(array) {
     if (array[i] > array[i + 1] && validNumbers.includes(absValue)) {
     } else {
       result = false;
+      break;
     }
   }
   return result;
@@ -49,7 +66,7 @@ async function main() {
   let data = await readFileInput();
   let newData = [];
   let countOriginalSafe = 0;
-  let countUpdated = 0;
+  let countUpdatedSafe = 0;
   let unSafeArrays = [];
   data.forEach((row) => {
     let result = isIncreasingOrDecreasing(row);
@@ -59,8 +76,27 @@ async function main() {
       unSafeArrays.push(row);
     }
   });
-
   console.log("Sum original safe rows: " + countOriginalSafe);
+
+  // Part 2
+
+  unSafeArrays.forEach((row) => {
+    let data = row.split(" ");
+    let Arr = data.map(Number);
+    for (let i = 0; i < Arr.length - 1; i++) {
+      let updatedArr = [...Arr];
+      updatedArr.splice(i, 1);
+      let result = isIncreasingOrDecreasingPart2(updatedArr);
+      if (result) {
+        countUpdatedSafe += 1;
+        break;
+      }
+    }
+  });
+
+  console.log("Sum updated safe rows: " + countUpdatedSafe);
+
+  console.log("Total Safe Rows: " + (countOriginalSafe + countUpdatedSafe));
 }
 
 main();
